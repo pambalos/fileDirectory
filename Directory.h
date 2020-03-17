b1c91afe7ccfa5beb2fc145dca4ab0b25f48c147
bjustice <bjustice@zuora.com>
2020-03-17 13:22:40 -0700

    directory stuff

--- Directory.h
+++ Directory.h
@@ -5,4 +5,15 @@
 #ifndef FILEDIRECTORY_DIRECTORY_H
 #define FILEDIRECTORY_DIRECTORY_H
 
+Typedef struct Dir_Entry {
+    char name[30];
+    char file_type[10];
+    unsigned char permissions;
+    unsigned long int date_created;
+    unsigned long int date_modified;
+    unsigned long long size;
+    unsigned long long location;
+}
+
+
 #endif //FILEDIRECTORY_DIRECTORY_H
