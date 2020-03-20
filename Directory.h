7cc0129bfefa717b50a52e77274993aeec1e8e4d
bjustice <bjustice@zuora.com>
2020-03-19 23:06:15 -0700

    updated 3/19 with free block trackers

--- Directory.h
+++ Directory.h
@@ -1,46 +1,40 @@
 //
 // Created by Justice, Bradley on 3/17/20.
 //
-/*
- Good clean structure
-
- What is file type?  is it a list of values or an arbitrary string?  Is it part of the file name or separate?
-
- Does this represent all you meta data?
-
- Do we want to enable clusters? - Think about waste, large files enable clustering, but lost of small files cluster is bad
-
- Waste = size_block/2 (larger blocks = more waste)
-*/
-
-
 #ifndef FILEDIRECTORY_DIRECTORY_H
 #define FILEDIRECTORY_DIRECTORY_H
+
+#include <stdint.h>
+
+#define BLOCK_SIZE 512
 
 enum FileType {
     txt, zip, sh, exe, pdf, bat
 };
 
 
-typedef struct Dir_Entry {
+struct Dir_Entry {
     char name[30];
     enum FileType file_type;
     unsigned char permissions;
     unsigned long int date_created;
     unsigned long int date_modified;
     unsigned long long size;
-
 };
 
-typedef struct File_System_Info {
+struct Free_Blocks {
+    long Num_Free_Blocks;
+    int_fast8_t Free_Blocks[];
+};
+
+struct File_System_Info {
     struct Dir_Entry *root;
-    struct Dir_Entry **LBA; //LBA points to our
-    struct Dir_Entry **Free_Blocks;
-    struct Dir_Entry **Free_Blocks2;
+    struct Dir_Entry **LBA;
     unsigned long int volume_size;
     char volume_name[30];
     unsigned int volume_id;
-    unsigned short int lba_size;
+    struct Free_Blocks *Free_Blocks;
+    struct Free_Blocks *Free_Blocks2;
 };
 
 #endif //FILEDIRECTORY_DIRECTORY_H
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main() {
 
     struct File_System_Info FileSystem;
+
     printf("Hello, World!\n");
     return 0;
 }
